d5d5eeb7bce66c16768da580ebb855bc774c2a72
unknown <makar.simonov@infis.cz>
2025-05-21 13:15:46 +0200

    SAVE

--- func.py
+++ func.py
@@ -1,3 +1,6 @@
 def f_print(word:str,count:int):
     for i in range(count):
         print(word)
+
+def first_p(word:str):
+    print(word[0])
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     word = input("Enter the word: ")
     count = int(input("Enter count: "))
     f_print(word,count)
+    first_p(word.replace(' ',''))
     
+  
 if __name__ == '__main__':
     main()
