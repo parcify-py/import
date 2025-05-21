93024629a10a27f919237268922d4096de4fc135
unknown <makar.simonov@infis.cz>
2025-05-21 13:00:04 +0200

    save

---
+++ func.py
@@ -0,0 +1,3 @@
+def f_print(word:str,count:int):
+    for i in range(count):
+        print(word)
---
+++ main.py
@@ -0,0 +1,9 @@
+from func import *
+
+def main():
+    word = input("Enter the word: ")
+    count = int(input("Enter count: "))
+    f_print(word,count)
+    
+if __name__ == '__main__':
+    main()
